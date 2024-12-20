6278a35d23686e59c71760dab865b37488f49203
hogo1510 <lucasfenstra@gmail.com>
2024-12-20 16:56:32 +0100

    httpServerTest.py gemaakt (het is verschikkelijk i know) httpclient class in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import httpClient.httpPythonClient;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lefz!");
+        httpPythonClient client = new httpPythonClient();
+
+        String json = client.getMessage();
+        System.out.println(json);
     }
 }
--- src/main/python/test/httpServer/httpServerTest.py
+++ src/main/python/test/httpServer/httpServerTest.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
-from httpServer import app
+from httpServer.httpServer import app
 
 client = TestClient(app)
-#tuurlijk werkt het niet. fkcing eerste keer test schrijven in python SOS HELP MIJ!!!
+
 def test_bonjour_handler():
     response = client.get("/bonjour")
     assert response.status_code == 200
